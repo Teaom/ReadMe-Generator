f0ec865decbfe4e5e81a3fdcd31f0f491d6bb3d7
Teaom <dclarkjenkins87@gmail.com>
2023-02-15 16:09:10 -0700

    Removed not needed lines of code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ## Github licence(https://img.shields.io/badge/license-${data.license}-green.svg)
